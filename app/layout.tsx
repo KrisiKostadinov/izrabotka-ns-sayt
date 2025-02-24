4230ade2ca9ae540ce67a16b230e1854d728702c
krisikostadinov <krisi.199898@gmail.com>
2025-02-24 13:45:12 +0200

    Add Google Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ThemeProvider } from "@/app/providers/theme-provider";
 import ToastProvider from "./providers/toaster-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
   return (
     <html lang="bg" className="dark" suppressHydrationWarning>
       <body>
+        <GoogleAnalytics gaId="G-7Z0THRT08Y"  />
         <ToastProvider>
           <ThemeProvider
             attribute="class"
